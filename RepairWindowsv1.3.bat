0d6a590385837f62e2f006ab0f34cfc0da292d8e
Little-Alexander-CS <77220782+Little-Alexander-CS@users.noreply.github.com>
2022-10-17 19:45:00 -0400

    Update RepairWindowsv1.3.bat

--- RepairWindowsv1.3.bat
+++ RepairWindowsv1.3.bat
@@ -2,7 +2,6 @@
 echo "Starting windows recovery batch file by Alexander Little..."
 
 :start
-rem set /A input=""
 echo "[1] - Basic DISM Scan"
 echo "[2] - Advanced DISM Scan"
 echo "[3] - SFC Scan"
@@ -12,6 +11,7 @@ echo "[6] - Defragment Drives"
 echo "[7] - Resource Monitor"
 echo "[8] - Task Manager"
 echo "[9] - DNS Flush/Renew"
+echo "[10] - Ping loopback IP / 1.1.1.1"
 set /p choicevar="Enter your choice: "
 
 if %choicevar% EQU 1 goto restorebasic
@@ -23,6 +23,7 @@ if %choicevar% EQU 6 goto defragment
 if %choicevar% EQU 7 goto resourcemonitor
 if %choicevar% EQU 8 goto taskmgr
 if %choicevar% EQU 9 goto dnsflush
+if %choicevar% EQU 10 goto pingip
 
 echo "Invalid Option, please try again"
 goto start 
@@ -30,6 +31,8 @@ goto start
 del /f /q "C:\Windows\Temp\"
 rem alexa must be replaced with account name
 del /f /q "C:\Users\alexa\AppData\Local\Temp\"
+start "" "C:\Windows\Temp\"
+start "" "C:\Users\alexa\AppData\Local\Temp\"
 echo "Finished..."
 goto end
 :restorebasic
@@ -67,6 +70,11 @@ cmd /c "ipconfig /flushdns"
 cmd /c "ipconfig /renew"
 cmd /c "ipconfig /displaydns"
 echo "Finished..."
+goto end
+:pingip
+cmd /c "ping 127.0.0.1"
+cmd /c "ping 1.1.1.1"
+cmd /c "ipconfig /all"
 goto end
 :end
 set /p startOver="Would you like to run another command? [1 - YES, OTHER - NO]: "
