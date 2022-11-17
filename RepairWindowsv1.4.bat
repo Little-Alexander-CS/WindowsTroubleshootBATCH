2c98a2d144ac4a00963ad3211509be6d14962a0a
Little-Alexander-CS <77220782+Little-Alexander-CS@users.noreply.github.com>
2022-11-17 18:55:23 -0500

    Add files via upload

---
+++ RepairWindowsv1.4.bat
@@ -0,0 +1,95 @@
+@echo off
+echo "Starting windows recovery batch file by Alexander Little..."
+
+:start
+echo "[1] - Basic DISM Scan"
+echo "[2] - Advanced DISM Scan"
+echo "[3] - SFC Scan"
+echo "[4] - Clear Cache"
+echo "[5] - Disk Cleanup"
+echo "[6] - Defragment Drives"
+echo "[7] - Resource Monitor"
+echo "[8] - Task Manager"
+echo "[9] - DNS Flush/Renew"
+echo "[10] - Ping loopback IP / 1.1.1.1"
+echo "[11] - Winget Update Service"
+set /p choicevar="Enter your choice: "
+
+if %choicevar% EQU 1 goto restorebasic
+if %choicevar% EQU 2 goto restoreadvanced
+if %choicevar% EQU 3 goto sfcscan
+if %choicevar% EQU 4 goto cacheclear
+if %choicevar% EQU 5 goto diskcleanup
+if %choicevar% EQU 6 goto defragment
+if %choicevar% EQU 7 goto resourcemonitor
+if %choicevar% EQU 8 goto taskmgr
+if %choicevar% EQU 9 goto dnsflush
+if %choicevar% EQU 10 goto pingip
+if %choicevar% EQU 11 goto update
+
+echo "Invalid Option, please try again"
+goto start 
+:cacheclear
+del /f /q "C:\Windows\Temp\"
+rem alexa must be replaced with account name
+del /f /q "C:\Users\alexa\AppData\Local\Temp\"
+start "" "C:\Windows\Temp\"
+start "" "C:\Users\alexa\AppData\Local\Temp\"
+echo "Finished..."
+goto end
+:restorebasic
+echo "Starting Basic DISM Scan..."
+cmd /c "DISM.exe /Online /Cleanup-Image /RestoreHealth"
+echo "Finished..."
+goto end
+:restoreadvanced
+echo "Starting Advanced DISM Scan..."
+cmd /c "DISM.exe /Online /Cleanup-Image /RestoreHealth /Source:C:\RepairSource\Windows"
+echo "Finished..."
+goto end
+:sfcscan
+cmd /c "sfc /scannow"
+echo "Finished..."
+goto end
+:diskcleanup
+start /d "C:\WINDOWS\system32" cleanmgr.exe
+echo "Finished..."
+goto end
+:defragment
+start /d "C:\WINDOWS\system32" dfrgui.exe
+echo "Finished..."
+goto end
+:resourcemonitor
+start /d "C:\WINDOWS\system32" perfmon.exe
+echo "Finished..."
+goto end
+:taskmgr
+start /d "C:\WINDOWS\system32" Taskmgr.exe
+echo "Finished..."
+goto end
+:dnsflush
+cmd /c "ipconfig /flushdns"
+cmd /c "ipconfig /renew"
+cmd /c "ipconfig /displaydns"
+echo "Finished..."
+goto end
+:pingip
+cmd /c "ping 127.0.0.1"
+cmd /c "ping 1.1.1.1"
+cmd /c "ipconfig /all"
+goto end
+:update
+echo "Checking for updates..."
+Powershell.exe winget upgrade --include-unknown
+echo "Finished..."
+set /p startOverPS="Would you like to proceed with these updates? [N - NO, YES - Y / OTHER]: "
+IF /i %startOverPS% EQU N goto start
+echo "Installing updates..."
+Powershell.exe winget upgrade --all --include-unknown
+echo "Finished..."
+goto end
+:end
+set /p startOver="Would you like to run another command? [1 - YES, OTHER - NO]: "
+IF /i %startOver% EQU 1 goto start
+echo "Finished...!"
+timeout -1 /nobreak
