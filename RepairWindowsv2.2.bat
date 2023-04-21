698c6746c91ad4aed8eeb76d21e221752b925976
Little-Alexander-CS <77220782+Little-Alexander-CS@users.noreply.github.com>
2023-04-21 09:21:08 -0400

    Update RepairWindowsv2.2.bat

--- RepairWindowsv2.2.bat
+++ RepairWindowsv2.2.bat
@@ -256,7 +256,7 @@ goto application_menu
 :dnsflush
 cmd /c "ipconfig /flushdns"
 cmd /c "ipconfig /renew"
-start cmd /k /c "ipconfig /displaydns"
+start cmd /k cmd /c "ipconfig /displaydns"
 echo "Finished..."
 pause
 goto network_menu
@@ -264,7 +264,7 @@ goto network_menu
 :pingip
 cmd /c "ping 127.0.0.1"
 cmd /c "ping 1.1.1.1"
-start cmd /k /c "ipconfig /all"
+start cmd /k cmd /c "ipconfig /all"
 pause
 goto network_menu
 
